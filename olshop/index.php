a71838872b5bd49c23c2a3ebb47634a9452dd2e3
mbuhrareti <65109182+mbuhrareti@users.noreply.github.com>
2020-05-10 15:21:20 +0700

    Add files via upload

---
+++ olshop/index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head> 
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+ 
+	<link rel="stylesheet" href="assets/css/bootstrap.css"> 
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />	
+    <title>Sayur Keranjang</title> 
+</head> 
+<body> 
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><a class="navbar-brand" href="#"><img src="img/Sayur keranjang.png" width="120" hight="40" class="d-inline-block align-top" alt=""/></a></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <form class="form-inline my-2 my-lg-0 justify-content-center">
+            <div class="input-group main">
+              <input type="text" class="form-control" placeholder="Cari">
+                <div class="input-group-append">
+                    <button class="btn btn-secondary" type="button">
+                    <i class="fa fa-search"></i>
+                    </button>
+                </div>
+             </div>
+        </form>
+
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Masuk</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Daftar</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+    </nav>
+
+	<script src="assets/js/jquery.js"></script> 
+	<script src="assets/js/popper.js"></script> 
+	<script src="assets/js/bootstrap.js"></script>
+</body> 
+</html>
+
